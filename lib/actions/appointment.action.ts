325ba020e54982e2ad68729bfe0abef5417c6f92
amankumar033 <amankumar51462@gmail.com>
2025-06-18 22:32:22 +0530

    updated

---
+++ lib/actions/appointment.action.ts
@@ -0,0 +1,69 @@
+"use server";
+
+import { ID, Query } from "node-appwrite";
+
+import {
+  BUCKET_ID,
+  DATABASE_ID,
+  ENDPOINT,
+  PATIENT_COLLECTION_ID,
+  PROJECT_ID,
+  databases,
+  APPOINTMENT_COLLECTION_ID,
+  storage,
+  users,
+  
+} from "../appwrite.config";
+import { parseStringify } from "../utils";
+interface RegisterPageProps {
+  params: {
+    userid: string;
+  };
+}
+
+export const createAppointment = async (
+  appointment: CreateAppointmentParams
+) => {
+  try {
+    const response = await databases.createDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      ID.unique(),
+      appointment
+    );
+    return response;
+  } catch (error) {
+    console.error("Error creating appointment:", error);
+    throw error;
+  }
+};
+export async function getAllAppointments() {
+  try {
+    const response = await databases.listDocuments(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      []
+    );
+    return response.documents;
+  } catch (error) {
+    console.error('Error fetching appointments:', error);
+    return [];
+  }
+}
+export async function updateAppointmentStatus(
+  appointmentId: string,
+  status: string
+) {
+  try {
+    const response = await databases.updateDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId,
+      { status }
+    );
+    return response;
+  } catch (error) {
+    console.error("Error updating appointment status:", error);
+    throw error;
+  }
+}
